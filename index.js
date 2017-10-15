1d6e96710c6c8da00c212d5a53c35d8c49b378fd
daswolke <wolke@weeb.sh>
2017-10-15 17:33:02 +0200

    this should work better

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ function RainCache(...args) {
     return new Raincache(...args);
 }
 
-module.exports.Connectors = {
+RainCache.Connectors = {
     AmqpConnector: require('./src/connector/AmqpConnector'),
     DirectConnector: require('./src/connector/DirectConnector'),
 };
 
-module.exports.Engines = {
+RainCache.Engines = {
     RedisStorageEngine: require('./src/storageEngine/RedisStorageEngine'),
 };
-module.exports.RainCache = Raincache;
 module.exports = RainCache;
