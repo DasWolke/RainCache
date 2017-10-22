32a322edced70e5f6b25c8ee5a2f9269b448ba04
rosiecode <rosiecode@outlook.com>
2017-10-22 11:11:53 +0100

    asdfasdf

--- src/connector/KafkaConnectorInbound.js
+++ src/connector/KafkaConnectorInbound.js
@@ -13,7 +13,7 @@ class KafkaConnectorInbound extends BaseConnector {
         Object.assign(this.options, options);
 
         this.params = {
-            // 'debug': 'all',
+            'debug': 'all',
             'metadata.broker.list': this.options.url,
             'message.max.bytes': '200000000',
             'group.id': 'node-rdkafka-consumer',
