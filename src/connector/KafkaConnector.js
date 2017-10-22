7a4d5832952da2cede8dad5f3584213ccfbce058
rosiecode <rosiecode@outlook.com>
2017-10-22 01:33:11 +0100

    thonk

--- src/connector/KafkaConnector.js
+++ src/connector/KafkaConnector.js
@@ -56,7 +56,7 @@ class KafkaConnector extends BaseConnector {
 
         this.channel.on('data', function (m) {
             console.log(`data owo`);
-            console.log(m.toString());
+            console.log(m.value.toString());
 
             counter++;
             //committing offsets every numMessages
