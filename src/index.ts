e7c512ea347db4f7cfdfd7fc2bf82f676ce11eda
Papa <bradleyjplatt@yahoo.com>
2021-11-06 22:11:25 -0700

    Finalize + docgen

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import BaseCache from "./cache/BaseCache";
+import BaseConnector from "./connector/BaseConnector";
+import AmqpConnector from "./connector/AmqpConnector";
+import DirectConnector from "./connector/DirectConnector";
+import BaseStorageEngine from "./storageEngine/BaseStorageEngine";
+import MemoryStorageEngine from "./storageEngine/MemoryStorageEngine";
+import RedisStorageEngine from "./storageEngine/RedisStorageEngine";
+
+import EventProcessor from "./EventProcessor";
+import RainCache from "./RainCache";
+
+import ChannelCache from "./cache/ChannelCache";
+import ChannelMapCache from "./cache/ChannelMapCache";
+import EmojiCache from "./cache/EmojiCache";
+import GuildCache from "./cache/GuildCache";
+import MemberCache from "./cache/MemberCache";
+import PermissionOverwriteCache from "./cache/PermissionOverwriteCache";
+import PresenceCache from "./cache/PresenceCache";
+import RoleCache from "./cache/RoleCache";
+import UserCache from "./cache/UserCache";
+import VoiceStateCache from "./cache/VoiceStateCache";
+
+export {
+	BaseCache,
+	ChannelCache,
+	ChannelMapCache,
+	EmojiCache,
+	GuildCache,
+	MemberCache,
+	PermissionOverwriteCache,
+	PresenceCache,
+	RoleCache,
+	UserCache,
+	VoiceStateCache,
+	BaseConnector,
+	AmqpConnector,
+	DirectConnector,
+	BaseStorageEngine,
+	MemoryStorageEngine,
+	RedisStorageEngine,
+	EventProcessor,
+	RainCache
+};
+
+export default exports as typeof import("./index"); // THIS ACTUALLY WORKS AND IS FUCKING CURSED
